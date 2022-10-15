cae2e507440a303e4bbb129aa9aad7f6116a6ee0
deepakdashcode <dipudash.2003@gmail.com>
2022-10-15 14:02:24 +0530

    updated

--- DynamicMemoryAllocationAnd2DArrays/danglingPointer.cpp
+++ DynamicMemoryAllocationAnd2DArrays/danglingPointer.cpp
@@ -1,15 +1,36 @@
 #include<iostream>
 using namespace std;
 
-int* fun()
-{
-    int x = 10;
-    return &x;
-}
+// int* fun()
+// {
+//     int x = 10;
+//     return &x;
+// }
 int main()
 {
-    int *ptr = fun();
-    cout << *ptr << endl;
+
+    // Case 1 : Functions
+    // int *ptr = fun();
+    // cout << *ptr << endl;
+
+
+
+    // Case 2 : Dynamlc memory a ocatlon.
+    // int *p = (int * ) malloc(sizeof(int));
+    // // after calling free() p becomes a dangling pointer
+    // free(p) ;
+    // //now p no more a dangling pointer.
+    // p = NULL;
+
+
+    // Case 3 : Variable goes out of scope
+
+    int *p = 0;
+    {
+        int a = 50;
+        p = &a;
+    }
+    cout << *p << endl;
     
 return 0;
 }
