8f657241c4bf867d87a2c5ca54fa58ebaaee2724
deepakdashcode <dipudash.2003@gmail.com>
2022-10-16 15:45:53 +0530

    updated

---
+++ globalVariables/first.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int score = 100;
+
+void printScore()
+{
+    cout << score << endl;
+}
+int main()
+{
+    printScore();
+    cout << score << endl;
+
+return 0;
+}
