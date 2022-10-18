a3e9397e89ea9ccd97aec5edff466e74f104c68f
deepakdashcode <dipudash.2003@gmail.com>
2022-10-18 15:24:34 +0530

    updated

--- Recursion/sayDigits.cpp
+++ Recursion/sayDigits.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 using namespace std;
+
+string arr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+void print(int n)
+{
+    if(n <= 9)
+    {
+        cout << arr[n] << " ";
+        return;
+    }
+    int lastDigit = n % 10;
+    print(n / 10);
+    cout << arr[lastDigit] << " ";
+
+}
+
+
 int main()
 {
-    
+    int num = 45123053;
+    print(num);
 return 0;
 }
