8cd18be3283491dedd1744cfdf8492967eaadf24
deepakdashcode <dipudash.2003@gmail.com>
2022-10-24 14:52:04 +0530

    ok

--- Recursion/Quick Sort (L6)/temp.cpp
+++ Recursion/Quick Sort (L6)/temp.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 
-
 void print(int *arr, int start, int end)
 {
     for(int i = start; i <= end; i++)
@@ -9,45 +8,28 @@ void print(int *arr, int start, int end)
     
     cout << "\n";
 }
-void sort(int *arr, int low, int high)
+void modify(int *arr, int low, int high, int pivotIntdex)
 {
-    if(low >= high)
-        return;
-    
     int s = low, e = high;
-    int mid = s + (e - s) / 2;
-    int pivot = arr[mid];
-
+    int pivot = arr[pivotIntdex];
     while(s <= e)
     {
         while(arr[s] < pivot)
-        {
             s ++;
-        }
         while(arr[e] > pivot)
-        {
             e --;
-        }
-
+        
         if(s <= e)
-        {
             swap(arr[s ++], arr[e --]);
-        }
-
     }
-
-    sort(arr, low, e);
-    sort(arr, s, high);
-
 
 }
 int main()
 {
-    int arr[] = {1, 7, 8, 1, 3, 8, 2, 1, 0, 4, 1, 1, 7, 8, 1, 3, 8, 2, 1, 0, 4, 1};
-    int n = 22;
-    sort(arr, 0, n - 1);
+    int arr[] = {1, 7, 8, 9, 3, 8, 2, 1, 0, 4, 1};
+    int n = 11;
+    modify(arr, 0, n - 1, 0);
     print(arr, 0, n - 1);
-
 
 return 0;
 }
