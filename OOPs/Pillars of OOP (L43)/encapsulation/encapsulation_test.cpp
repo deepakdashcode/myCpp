fce92724c6fc4aa5006d23ab561ae1d17f74a861
deepakdashcode <dipudash.2003@gmail.com>
2022-11-01 13:05:16 +0530

    ok

---
+++ OOPs/Pillars of OOP (L43)/encapsulation/encapsulation_test.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+    private:
+        string name;
+        int age;
+        int height;
+    
+    public:
+    int getAge()
+    {
+        return this -> age;
+    }
+
+};
+
+int main()
+{
+    student first;
+    cout << "Finished\n";
+
+return 0;
+}
