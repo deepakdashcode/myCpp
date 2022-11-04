7e5d27b39efb2fec134784572e133a2c82d8cb91
deepakdashcode <dipudash.2003@gmail.com>
2022-11-04 14:22:20 +0530

    ok

--- OOPs/Pillars of OOP (L43)/Inheritance/Types of Inhertitance/multiple_inheritance.cpp
+++ OOPs/Pillars of OOP (L43)/Inheritance/Types of Inhertitance/multiple_inheritance.cpp
@@ -1,9 +1,44 @@
 #include<iostream>
 using namespace std;
 
+class animal
+{
+    public:
+    int age;
+    int weight;
 
-int main()
+    void bark()
+    {
+        cout << "Barking\n";
+    }
+};
+
+class human
+{
+    public:
+    string color;
+
+    void speak()
+    {
+        cout << "Speaking\n";
+    }
+
+};
+
+class hybrid : public animal, public human
 {
     
+    public:
+    void tell()
+    {
+        speak();
+        bark();
+    }
+
+};
+int main()
+{
+    hybrid a;
+    a.tell();
 return 0;
 }
